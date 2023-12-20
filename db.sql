49ebc08113c2057a9335eccb78788b35f8748433
luoyuhou <luoyuhou@qq.com>
2023-12-20 21:52:56 +0800

    info: create order & category & goods table

--- db.sql
+++ db.sql
@@ -98,3 +98,97 @@ CREATE TABLE store_history (
     INDEX `store_history_idx` (`store_id`) USING BTREE,
     INDEX `store_action_type_idx` (`action_type`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE category_goods (
+    id int unsigned NOT NULL AUTO_INCREMENT,
+    category_id varchar(64) NOT NULL,
+    store_id varchar(64) NOT NULL,
+    pid varchar(64),
+    name varchar(16) NOT NULL,
+    status tinyint(4) NOT NULL,
+    create_date datetime DEFAULT CURRENT_TIMESTAMP,
+    update_date datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY (`category_id`),
+    UNIQUE KEY (`store_id`, `name`) USING BTREE,
+    INDEX `category_goods_name` (`name`) USING BTREE,
+    INDEX `category_goods_idx` (`store_id`, `pid`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE store_goods (
+    id int unsigned NOT NULL AUTO_INCREMENT,
+    goods_id varchar(64) NOT NULL,
+    store_id varchar(64) NOT NULL,
+    category_id varchar(64) NOT NULL,
+    name varchar(16) NOT NULL,
+    description varchar(256),
+    status tinyint(4) NOT NULL,
+    create_date datetime DEFAULT CURRENT_TIMESTAMP,
+    update_date datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY (`goods_id`),
+    INDEX `store_idx` (`store_id`) USING BTREE,
+    INDEX `goods_name_idx` (`name`) USING BTREE,
+    UNIQUE KEY (`store_id`, `category_id`, `name`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE store_goods_version (
+    id int unsigned NOT NULL AUTO_INCREMENT,
+    version_id varchar(64) NOT NULL,
+    goods_id varchar(64) NOT NULL,
+    version_number varchar(32),
+    bar_code varchar(32),
+    count int unsigned NOT NULL,
+    price int unsigned NOT NULL,
+    supplier varchar(64),
+    create_date datetime DEFAULT CURRENT_TIMESTAMP,
+    update_date datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY (`version_id`),
+    UNIQUE KEY (`goods_id`, `version_number`),
+    UNIQUE KEY (`goods_id`, `bar_code`),
+    INDEX `goods_idx` (`goods_id`) USING BTREE,
+    INDEX `goods_suppler_idx` (`supplier`) USING BTREE,
+    INDEX `goods_version_number_idx` (`version_number`) USING BTREE,
+    INDEX `goods_bar_code_idx` (`bar_code`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE user_order (
+    id int unsigned NOT NULL AUTO_INCREMENT,
+    order_id varchar(64) NOT NULL,
+    user_id varchar(64) NOT NULL,
+    store_id varchar(64) NOT NULL,
+    status tinyint(4) NOT NULL,
+    recipient varchar(16) NOT NULL,
+    money int unsigned NOT NULL,
+    phone varchar(16) NOT NULL,
+    province varchar(8) NOT NULL,
+    city varchar(8) NOT NULL,
+    district varchar(16) NOT NULL,
+    address varchar(128) NOT NULL,
+    create_date datetime DEFAULT CURRENT_TIMESTAMP,
+    update_date datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY (`order_id`),
+    INDEX `user_order_user_idx` (`user_id`) USING BTREE,
+    INDEX `user_order_store_idx` (`store_id`) USING BTREE,
+    INDEX `user_order_idx` (`user_id`, `store_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE user_order_info (
+    id int unsigned NOT NULL AUTO_INCREMENT,
+    order_info_id varchar(64) NOT NULL,
+    order_id varchar(64) NOT NULL,
+    goods_id varchar(64) NOT NULL,
+    goods_name varchar(16) NOT NULL,
+    goods_version_id varchar(64) NOT NULL,
+    count int unsigned NOT NULL,
+    price int unsigned NOT NULL,
+    create_date datetime DEFAULT CURRENT_TIMESTAMP,
+    update_date datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY (`order_info_id`),
+    INDEX `order_Info_order_idx` (`order_id`) USING BTREE,
+    INDEX `order_info_goods_idx` (`goods_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
