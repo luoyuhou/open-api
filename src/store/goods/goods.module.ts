026e506fc3e259e4a6066f40f93d36dfb9c57743
luoyuhou <luoyuhou@qq.com>
2024-01-05 22:37:23 +0800

    build: fix faild for run project

--- src/store/goods/goods.module.ts
+++ src/store/goods/goods.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GoodsService } from './goods.service';
 import { GoodsController } from './goods.controller';
+import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [GoodsController],
-  providers: [GoodsService]
+  providers: [GoodsService],
 })
 export class GoodsModule {}
--- src/users/address/address.module.ts
+++ src/users/address/address.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AddressService } from './address.service';
 import { AddressController } from './address.controller';
+import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [AddressController],
   providers: [AddressService],
 })
