a32ac2608db1799d6f26c6b278b42610ffa517dd
luoyuhou <luoyuhou@qq.com>
2024-08-21 23:45:06 +0800

    chore: add unit test case for user service

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { WxUserInfo } from '../auth/dto/login.dto';
 
 describe('UsersService', () => {
   let service: UsersService;
@@ -13,7 +14,21 @@ describe('UsersService', () => {
     service = module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
+  it('Create User by Wechat', async () => {
     expect(service).toBeDefined();
+
+    const openId = Math.random().toString().slice(2, 15);
+    const userInfo: WxUserInfo = {
+      avatarUrl: '',
+      city: '',
+      country: '',
+      gender: 0,
+      language: 'zh_CN',
+      nickName: `nickName-${openId}`,
+      province: '',
+    };
+
+    const user = await service.createByWechat(userInfo, openId);
+    expect(user?.first_name).toBe(`nickName-${openId}`);
   });
 });
